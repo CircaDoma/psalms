c7e164b0190af943afe33d69486ec210ea2a8d80
CircaDoma <kaleb.farar@snapone.com>
2026-05-16 11:21:57 -0600

    psalm progress

---
+++ psalms.js
@@ -0,0 +1,14 @@
+const psalms = {
+    1: `
+        <p class="psalm-superscription">Blessed are the righteous—The ungodly will perish.</p>
+        <p><span class="verse-number">1</span> Blessed is the man that walketh not in the counsel of the ungodly, nor standeth in the way of sinners, nor sitteth in the seat of the scornful.</p>
+        <p><span class="verse-number">2</span> But his delight is in the law of the Lord; and in his law doth he meditate day and night.</p>
+        <p><span class="verse-number">3</span> And he shall be like a tree planted by the rivers of water, that bringeth forth his fruit in his season; his leaf also shall not wither; and whatsoever he doeth shall prosper.</p>
+        <p><span class="verse-number">4</span> The ungodly are not so: but are like the chaff which the wind driveth away.</p>
+        <p><span class="verse-number">5</span> Therefore the ungodly shall not stand in the judgment, nor sinners in the congregation of the righteous.</p>
+        <p><span class="verse-number">6</span> For the Lord knoweth the way of the righteous: but the way of the ungodly shall perish.</p>`,
+    2: `
+        <p class="psalm-superscription">...</p>
+        <p><span class="verse-number">1</span> ...</p>
+    `
+};
